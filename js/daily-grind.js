80f868d67c002e974cab311ca7f3893c9c360b50
Replit user <>
2023-06-05 20:39:18 +0000

    yes

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -11,6 +11,18 @@
 
 */
 
+function coffeeTemplate(coffee){
+return `
+
+<p>
+    		 <img src="images/pumpkin-spice-latte.jpg" alt="Our Pumpkin Spice Latte tastes great on a Fall Day!" id="coffee" />
+            <strong class="feature">Monday's Coffee Special:</strong> Monday's daily coffee special is <strong class="feature">Pumpkin Spice Latte</strong>, which makes us wish it was always Fall, as this is one of our top sellers!</p>
+
+`;
+
+  
+}
+
 let myDate = new Date();
 
 let today = myDate.getDay();
@@ -18,28 +30,33 @@ let today = myDate.getDay();
 //today =3;
 
 switch(today){
+
+    case 1:
+        today = "Monday";
+        coffee = {
+          name:"Bubble Tea",
+          color: "pink",
+          pic: "images/bubble-tea.jpg",
+          alt: "A picture of a bubble tea",
+          day: "Wednesday",
+          desc: `I like me some bubble tea!`
+        };
+    break;
     
     case 2:
-        today = "Tuesday"
+        today = "Tuesday";
     break;
 
     case 3:
-        today = "Wednesday"
+        today = "Wednesday";
     break;
 
     default:
-        today = "Not sure what day it is!"
+        today = "Not sure what day it is!";
 
 }
 
-coffee = {
-    name:"Bubble Tea",
-    color: "pink",
-    pic: "images/bubble-tea.jpg",
-    alt: "A picture of a bubble tea",
-    day: "Wednesday",
-    desc: `I like me some bubble tea!`
-}
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
 
 
 
